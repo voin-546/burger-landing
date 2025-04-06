ca9135f5e571f7139f6a1c95339ec7f82177264c
voin-546 <murodd108546@gmail.com>
2025-04-06 22:42:55 +0300

    Added some animations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-const scroll = new LocomotiveScroll({ el: document.querySelector("[data-scroll-container]"), smooth: true, tablet: { smooth: true }, smartphone: { smotth: true }, });
+AOS.init({
+    duration: 1200
+})
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  tablet: { smooth: true },
+  smartphone: { smotth: true },
+});
